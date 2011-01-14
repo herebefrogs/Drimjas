5d2401f1b5ad866eca265ef5185664a6e061e188
Jerome Lecomte <jerle@free.fr>
2011-01-13 21:37:55 -0500

    initial commit based on tab bar application template

---
+++ iPhone/WorkingTitle/Classes/FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  WorkingTitle
+//
+//  Created by Jerome Lecomte on 11-01-13.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FirstViewController : UIViewController {
+
+}
+
+@end
---
+++ iPhone/WorkingTitle/Classes/WorkingTitleAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  WorkingTitleAppDelegate.h
+//  WorkingTitle
+//
+//  Created by Jerome Lecomte on 11-01-13.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WorkingTitleAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
+    UIWindow *window;
+    UITabBarController *tabBarController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
