0d2e6fc6e052bbce9087ce985289a12d21ed37b8
Jerome Lecomte <jerle@free.fr>
2011-01-25 22:48:54 -0500

    allow any orientation in existing views (portrait, landscape, upside down...)

--- iPhone/WorkingTitle/Classes/UI/AddEstimateViewController.m
+++ iPhone/WorkingTitle/Classes/UI/AddEstimateViewController.m
@@ -50,13 +50,13 @@
     [super viewDidDisappear:animated];
 }
 */
-/*
+
 // Override to allow orientations other than the default portrait orientation.
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     // Return YES for supported orientations.
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
-*/
+
 
 
 #pragma mark -
--- iPhone/WorkingTitle/Classes/UI/EstimatesViewController.m
+++ iPhone/WorkingTitle/Classes/UI/EstimatesViewController.m
@@ -55,13 +55,12 @@
     [super viewDidDisappear:animated];
 }
 */
-/*
+
 // Override to allow orientations other than the default portrait orientation.
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     // Return YES for supported orientations.
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
-*/
 
 
 #pragma mark -
