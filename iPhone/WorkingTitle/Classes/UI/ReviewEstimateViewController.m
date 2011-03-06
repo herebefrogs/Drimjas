c6910bd23d5e58604961275153ec8cec851446b1
Jerome Lecomte <jerle@free.fr>
2011-03-06 10:45:29 -0500

    fix autorotation could not be initiated from ReviewEstimate screen

--- iPhone/WorkingTitle/Classes/UI/ReviewEstimateViewController.m
+++ iPhone/WorkingTitle/Classes/UI/ReviewEstimateViewController.m
@@ -74,13 +74,11 @@
     [super viewDidDisappear:animated];
 }
 */
-/*
+
 // Override to allow orientations other than the default portrait orientation.
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations.
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
-*/
 
 
 #pragma mark -
