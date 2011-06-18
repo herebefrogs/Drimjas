90eb99344b2a50d886b0c88dc42deb0e26c182ff
Jerome Lecomte <jerle76@gmail.com>
2011-06-17 22:21:51 -0400

    add empty Contracts screen

---
+++ iPhone/WorkingTitle/Classes/Views/ContractsViewController.h
@@ -0,0 +1,16 @@
+//
+//  ContractsViewController.h
+//  WorkingTitle
+//
+//  Created by Jerome Lecomte on 11-06-17.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ContractsViewController : UITableViewController {
+
+}
+
+@end
